dd2358c49b6ac30d4fbded3e8ae706a39a7f80e9
nzl-architecture <3353366044@qq.com>
2026-07-13 06:57:12 +0800

    Update start.bat

--- Release/sparse-pkg/start.bat
+++ Release/sparse-pkg/start.bat
@@ -1,3 +1,7 @@
+if %*= ""(
 echo "Welcome to use Appx Package Installer Community!"
-set /p "appxfile=Enter your appx package to install (allowed .appx .msix .appxbundle .msixbundle) :"
+set /p "appxfile=Enter your appx package to install (allowed .appx .msix .appxbundle .msixbundle) :")
+else (
+set "appxfile=%*"
+)
 AddAppxPackage.bat %appxfile%
